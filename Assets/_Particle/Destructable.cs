7e5db1a251ecde9dab17f7b7a63dfaa09028f2dd
戸澤海智 <st37071556@icloud.com>
2021-09-13 12:05:00 +0900

    月曜

---
+++ Assets/_Particle/Destructable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField] GameObject m_effect = default;
+
+    public void Hit()
+    {
+        if (m_effect)
+        {
+            Instantiate(m_effect, this.transform.position, Quaternion.identity);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/_Particle/PlayerAttackController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 攻撃を制御するコンポーネント
+/// </summary>
+public class PlayerAttackController : MonoBehaviour
+{
+    /// <summary>攻撃範囲の中心</summary>
+    [SerializeField] Vector2 m_offset = Vector2.right;
+    /// <summary>攻撃範囲の大きさ</summary>
+    [SerializeField] Vector2 m_size = Vector2.one;
+    Animator m_anim = default;
+    SpriteRenderer m_sprite = default;
+
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+        m_sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_anim.SetBool("Attack", true);
+        }
+    }
+
+    /// <summary>
+    /// 攻撃範囲の gizmo を表示する（選択時のみ）
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Vector2 centerOfAttackRange = (Vector2) this.transform.position + m_offset;
+
+        // 実行中は、左を向いている時は左に gizmo を表示する
+        if (m_sprite)
+        {
+            centerOfAttackRange = (Vector2)this.transform.position + m_offset * (m_sprite.flipX ? -1 : 1);
+        }
+
+        Gizmos.DrawWireCube(centerOfAttackRange, m_size);
+    }
+
+    /// <summary>
+    /// 攻撃する。Animation Event から呼び出すことを前提に作っている。
+    /// </summary>
+    void Attack()
+    {
+        Vector2 centerOfAttackRange = (Vector2)this.transform.position + m_offset * (m_sprite.flipX ? -1 : 1);
+        var cols = Physics2D.OverlapBoxAll(centerOfAttackRange, m_size, 0);
+
+        foreach (var c in cols)
+        {
+            var destruct = c.gameObject.GetComponent<Destructable>();
+            
+            if (destruct)
+            {
+                Debug.Log($"Hit {c.name}");
+                destruct.Hit();
+            }
+        }
+
+        m_anim.SetBool("Attack", false);
+    }
+}
